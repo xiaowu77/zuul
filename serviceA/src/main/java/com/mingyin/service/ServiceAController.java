694066aeb0e0006a4356a9156f69458c95fc43a2
xiaowu77 <2678713877@qq.com>
2019-11-04 11:31:36 +0800

    zuul

---
+++ serviceA/src/main/java/com/mingyin/service/ServiceAController.java
@@ -0,0 +1,35 @@
+package com.mingyin.service;
+
+import com.xiaowu.api.ServiceAInterface;
+import com.xiaowu.api.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ServiceAController implements ServiceAInterface {
+
+    public String sayHello(@PathVariable("id") Long id,
+                           @RequestParam("name") String name,
+                           @RequestParam("age") Integer age) {
+        System.out.println("打招呼，id=" + id + ", name=" + name + ", age=" + age);
+        return "{'msg': 'hello, " + name + "'}";
+    }
+    public String createUser(@RequestBody User user) {
+        System.out.println("创建用户，" + user);
+        return "{'msg': 'success'}";
+    }
+
+    public String updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+        System.out.println("更新用户，" + user);
+        return "{'msg': 'success'}";
+    }
+
+    public String deleteUser(@PathVariable("id") Long id) {
+        System.out.println("删除用户，id=" + id);
+        return "{'msg': 'success'}";
+    }
+
+    public User getById(@PathVariable("id") Long id) {
+        System.out.println("查询用户，id=" + id);
+        return new User(1L, "张三", 20);
+    }
+}
---
+++ serviceB/src/main/java/com/mingyin/service/ServiceAClient.java
@@ -0,0 +1,48 @@
+package com.mingyin.service;
+
+import com.xiaowu.api.ServiceAInterface;
+import com.xiaowu.api.User;
+import feign.hystrix.FallbackFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+
+@FeignClient(value="ServiceA",
+        fallbackFactory = ServiceAClient.ServiceAClientFallbackFactory.class)
+public interface ServiceAClient extends ServiceAInterface {
+    @Component
+    static class ServiceAClientFallbackFactory implements FallbackFactory<ServiceAClient> {
+
+        @Override
+        public ServiceAClient create(Throwable throwable) {
+            //每次要降级的时候，可以根据异常来区别处理
+
+            return new ServiceAClient() {
+                @Override
+                public String sayHello(Long id, String name, Integer age) {
+                    System.out.println("sayHello()方法的降级机制");
+                    return null;
+                }
+
+                @Override
+                public String createUser(User user) {
+                    return null;
+                }
+
+                @Override
+                public String updateUser(Long id, User user) {
+                    return null;
+                }
+
+                @Override
+                public String deleteUser(Long id) {
+                    return null;
+                }
+
+                @Override
+                public User getById(Long id) {
+                    return null;
+                }
+            };
+        }
+    }
+}
